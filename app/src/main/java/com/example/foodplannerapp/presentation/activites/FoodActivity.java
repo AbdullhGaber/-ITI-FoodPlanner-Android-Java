784bf554dd1ac8320db7e5c06d687a7694ec050f
AbdullhGaber <abdullh760@yahoo.com>
2026-02-04 02:09:31 +0200

    + Adding FoodNavGraph and put the NavHostFragment inside FoodActivity

---
+++ app/src/main/java/com/example/foodplannerapp/presentation/activites/FoodActivity.java
@@ -0,0 +1,40 @@
+package com.example.foodplannerapp.presentation.activites;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.foodplannerapp.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class FoodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_food);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.food_main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.food_nav_host_fragment);
+
+        if (navHostFragment != null) {
+            NavController navController = navHostFragment.getNavController();
+            NavigationUI.setupWithNavController(bottomNav, navController);
+        }
+    }
+}
