a97847c09c84e5254554261dc08be8e773dc4797
AbdullhGaber <abdullh760@yahoo.com>
2026-02-06 01:50:31 +0200

    ~ Remove slash from /random end point

--- app/src/main/java/com/example/foodplannerapp/data/network/MealService.java
+++ app/src/main/java/com/example/foodplannerapp/data/network/MealService.java
@@ -1,9 +1,11 @@
 package com.example.foodplannerapp.data.network;
 
+import com.example.foodplannerapp.data.model.MealResponse;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface MealService {
-    @GET("/random")
-    Call<Object> getRandomMeal();
+    @GET("random.php")
+    Call<MealResponse> getRandomMeal();
 }
