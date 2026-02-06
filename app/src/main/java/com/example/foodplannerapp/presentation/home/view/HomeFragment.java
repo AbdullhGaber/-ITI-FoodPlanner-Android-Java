078ba96179b569ee69bed5b2b9b81501b1722a7e
AbdullhGaber <abdullh760@yahoo.com>
2026-02-06 16:31:21 +0200

    + Add @AndroidEntryPoint in HomeFragment and Inject HomePresenter in it

--- app/src/main/java/com/example/foodplannerapp/presentation/home/view/HomeFragment.java
+++ app/src/main/java/com/example/foodplannerapp/presentation/home/view/HomeFragment.java
@@ -11,19 +11,25 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.foodplannerapp.R;
+import com.example.foodplannerapp.presentation.home.presenter.HomePresenter;
 
+import javax.inject.Inject;
 
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
 public class HomeFragment extends Fragment implements HomeView{
+    @Inject
+    HomePresenter homePresenter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
     }
 }
