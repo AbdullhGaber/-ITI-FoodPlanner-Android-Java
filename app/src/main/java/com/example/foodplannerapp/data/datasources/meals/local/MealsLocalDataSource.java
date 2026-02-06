07349d63fa3811458bde085f8c68f7743bce1d50
AbdullhGaber <abdullh760@yahoo.com>
2026-02-06 18:01:48 +0200

    + Add insertMeal and getMeals methods in local datasource

--- app/src/main/java/com/example/foodplannerapp/data/datasources/meals/local/MealsLocalDataSource.java
+++ app/src/main/java/com/example/foodplannerapp/data/datasources/meals/local/MealsLocalDataSource.java
@@ -1,4 +1,8 @@
 package com.example.foodplannerapp.data.datasources.meals.local;
 
+import com.example.foodplannerapp.data.db.meals.entities.Meal;
+
 public interface MealsLocalDataSource {
+    void getMeals();
+    void insertMeal(Meal meal);
 }
--- app/src/main/java/com/example/foodplannerapp/data/datasources/meals/local/MealsLocalDataSourceImpl.java
+++ app/src/main/java/com/example/foodplannerapp/data/datasources/meals/local/MealsLocalDataSourceImpl.java
@@ -1,8 +1,19 @@
 package com.example.foodplannerapp.data.datasources.meals.local;
-
+import com.example.foodplannerapp.data.db.meals.dao.MealDao;
+import com.example.foodplannerapp.data.db.meals.entities.Meal;
 import javax.inject.Inject;
 
 public class MealsLocalDataSourceImpl implements MealsLocalDataSource {
+    private final MealDao mealDao;
+
     @Inject
-    public MealsLocalDataSourceImpl() { }
+    public MealsLocalDataSourceImpl(MealDao mealDao) {
+        this.mealDao = mealDao;
+    }
+
+    @Override
+    public void getMeals() { }
+
+    @Override
+    public void insertMeal(Meal meal) { }
 }
