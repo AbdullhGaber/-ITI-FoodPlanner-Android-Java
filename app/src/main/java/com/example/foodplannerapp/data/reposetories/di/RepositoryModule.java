26d115535d4a601c7260e92e2962dffe284fa9d3
AbdullhGaber <abdullh760@yahoo.com>
2026-02-06 16:27:53 +0200

    + Add Repository Module

---
+++ app/src/main/java/com/example/foodplannerapp/data/reposetories/di/RepositoryModule.java
@@ -0,0 +1,29 @@
+package com.example.foodplannerapp.data.reposetories.di;
+
+import com.example.foodplannerapp.data.reposetories.auth.login.repository.LoginRepository;
+import com.example.foodplannerapp.data.reposetories.auth.login.repository.LoginRepositoryImpl;
+import com.example.foodplannerapp.data.reposetories.auth.register.repository.RegisterRepository;
+import com.example.foodplannerapp.data.reposetories.auth.register.repository.RegisterRepositoryImpl;
+import com.example.foodplannerapp.data.reposetories.meals.MealsRepository;
+import com.example.foodplannerapp.data.reposetories.meals.MealsRepositoryImpl;
+
+import dagger.Binds;
+import dagger.Module;
+import dagger.hilt.InstallIn;
+import dagger.hilt.components.SingletonComponent;
+
+
+@Module
+@InstallIn(SingletonComponent.class)
+public interface RepositoryModule {
+
+    //Auth
+    @Binds
+    LoginRepository getLoginRepository(LoginRepositoryImpl loginRepository);
+    @Binds
+    RegisterRepository getRegisterRepository(RegisterRepositoryImpl registerRepository);
+
+    //Meals
+    @Binds
+    MealsRepository getMealsRepository(MealsRepositoryImpl mealsRepository);
+}
