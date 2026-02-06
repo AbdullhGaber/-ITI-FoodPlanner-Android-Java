b0798755f4655933fa8082f9a0fd21c8aa3615c8
AbdullhGaber <abdullh760@yahoo.com>
2026-02-06 16:32:51 +0200

    ~ Inject MealRepository and implement getRandomMeal() in HomePresenterImpl

--- app/src/main/java/com/example/foodplannerapp/presentation/home/presenter/HomePresenterImpl.java
+++ app/src/main/java/com/example/foodplannerapp/presentation/home/presenter/HomePresenterImpl.java
@@ -1,3 +1,20 @@
 package com.example.foodplannerapp.presentation.home.presenter;
 
-public class HomePresenterImpl implements HomePresenter{ }
+import com.example.foodplannerapp.data.reposetories.meals.MealsRepository;
+
+
+import javax.inject.Inject;
+
+public class HomePresenterImpl implements HomePresenter {
+    private final MealsRepository mealsRepository;
+
+    @Inject
+    public HomePresenterImpl(MealsRepository mealsRepository) {
+        this.mealsRepository = mealsRepository;
+    }
+
+    @Override
+    public void getRandomMeal() {
+        mealsRepository.getRandomMeal();
+    }
+}
