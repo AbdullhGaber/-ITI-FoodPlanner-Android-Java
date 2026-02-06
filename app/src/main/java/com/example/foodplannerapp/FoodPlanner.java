74450748342edbdbbb8b18ae25e77e5113748936
AbdullhGaber <abdullh760@yahoo.com>
2026-02-06 16:23:00 +0200

    + Add DI App Class

---
+++ app/src/main/java/com/example/foodplannerapp/FoodPlanner.java
@@ -0,0 +1,8 @@
+package com.example.foodplannerapp;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class FoodPlanner extends Application {}
